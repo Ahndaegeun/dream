fd85f60dac560b6e3933fc7f86d30c98f1cd3ce7
kade <adg0807@naver.com>
2021-04-11 23:44:36 +0900

    007 ing

---
+++ Chapter_1/position1/main.js
@@ -0,0 +1,10 @@
+const _target = document.querySelector('.target');
+const _horizontal = document.querySelector('.horizontal-line');
+const _vertical = document.querySelector('.vertical-line');
+
+window.addEventListener('mousemove', (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+  console.log(x,y);
+  
+})
