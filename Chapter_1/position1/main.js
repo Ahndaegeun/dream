d0c2c947d2521e1cc1eb8e335eb8d6065c9a56f4
kade <adg0807@naver.com>
2021-04-13 23:05:10 +0900

    0413

--- Chapter_1/position1/main.js
+++ Chapter_1/position1/main.js
@@ -6,12 +6,19 @@ const _sign = document.querySelector('.sign');
 let x = 0;
 let y = 0;
 
-window.addEventListener('mousemove', (e) => {
-  x = e.clientX;
-  y = e.clientY;
-  _target.style.top = `${y}px`;
-  _target.style.left = `${x}px`;
-  _horizontal.style.top = `${y}px`;
-  _vertical.style.left = `${x}px`;
-  _sign.innerHTML = `${x}px, ${y}px`;
+window.addEventListener('load', () => {
+  const _targetRect = _target.getBoundingClientRect();
+  window.addEventListener('mousemove', (e) => {
+    x = e.clientX;
+    y = e.clientY;
+  
+    //_target.style.top = `${y}px`;
+    //_target.style.left = `${x}px`;
+    _target.style.transform = `translate(${x-_targetRect.width/2}px, ${y-_targetRect.height/2}px)`
+    // _horizontal.style.top = `${y}px`;
+    // _vertical.style.left = `${x}px`;
+    _horizontal.style.transform = `translateY(${y}px)`;
+    _vertical.style.transform = `translateX(${x}px)`;
+    _sign.innerHTML = `${x}px, ${y}px`;
+  })
 })
