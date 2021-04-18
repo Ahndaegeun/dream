d519f26a477a7c57ba1b541ad453917113e42e4e
kade <adg0807@naver.com>
2021-04-18 10:20:47 +0900

    carrot

--- Chapter_1/dreamShop/main.js
+++ Chapter_1/dreamShop/main.js
@@ -15,27 +15,21 @@ function onAdd() {
   input.focus();
 }
 
+//고유값을 아이디로 주려면 UUID 같은 라이브러리를 참고
+let id = 0;
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fa fa-trash"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  })
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
-
+  itemRow.setAttribute('data-id', id)
+  itemRow.innerHTML = `
+    <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="item__delete"><i class="fa fa-trash" data-id="${id}"></i></button>
+      </div>
+      <div class="divider">
+    </div>
+  `;
+  id++;
   return itemRow;
 }
 
@@ -46,5 +40,13 @@ addBtn.addEventListener('click', () => {
 input.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     onAdd();
+  }
+})
+
+items.addEventListener('click', e => {
+  const id = e.target.dataset.id
+  if(id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 })
